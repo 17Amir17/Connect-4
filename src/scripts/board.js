987387672bd505624d8d5df20ce7fb7841152358
17Amir17 <amirangel101@hotmail.com>
2021-10-30 15:52:53 +0300

    updated dist

--- src/scripts/board.js
+++ src/scripts/board.js
@@ -13,6 +13,7 @@ export class Board {
     boardElement.setAttribute('id', 'board');
     parentElement.appendChild(boardElement);
     if (sizeX < 6 || sizeY < 6) throw 'Board is too small min size is 6';
+    if (sizeX > 20 || sizeX > 20) throw 'Board is too big max size is 20';
     for (let y = 0; y < sizeY; y++) {
       const row = Board.#createRowElement(y);
       boardElement.appendChild(row);
